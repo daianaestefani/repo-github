3709691cf78dc182cacb67ac8d13f00756e015d8
daianaestefani <69016478+daianaestefani@users.noreply.github.com>
2020-08-14 14:00:53 -0300

    Update login.js
    modificando login.js, debido que lo habia hecho con errores al escribir el codigo para que abra el menu principal

--- js/login.js
+++ js/login.js
@@ -1,15 +1,15 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-});
 
-function loginuser(user,pass){
+function loginUser(user, pass){
     if (user.trim()==="" || pass.trim()===""){
-        alert("Debe rellenar todos los datos para continuar");
-    }   else {
-            localStorage.setItem("usuario",user.trim());
-            localStorage.setItem("contraseña",pass.trim());
-            location.href="principalmenu.html";
+        alert("Debe rellenar los datos");
+    }else {
+        localStorage.setItem("usuario", user.trim());
+        localStorage.setItem("contraseña", pass.trim());
+        location.href="principalmenu.html";
     }
 }
+/*ocument.addEventListener("DOMContentLoaded", function(e){
+});*/
