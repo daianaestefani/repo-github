4d1e01c0164a038f33982cfa1c8010ef72a0c0d2
daianaestefani <69016478+daianaestefani@users.noreply.github.com>
2020-08-16 04:02:15 -0300

    Update login.js
    modificar login, para abrir pantalla principal

--- js/login.js
+++ js/login.js
@@ -1,15 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
-function loginUser(user, pass){
-    if (user.trim()==="" || pass.trim()===""){
-        alert("Debe rellenar los datos");
-    }else {
-        localStorage.setItem("usuario", user.trim());
-        localStorage.setItem("contraseña", pass.trim());
-        location.href="principalmenu.html";
-    }
-}
-/*ocument.addEventListener("DOMContentLoaded", function(e){
+/*document.addEventListener("DOMContentLoaded", function(e){
 });*/
+function loginUser(user, pass){
+    if (user.trim()=== "" || pass.trim()=== ""){
+        alert("Debe rellenar los datos");
+    } else {
+        /*localStorage.setItem("usuario", user.trim());
+        localStorage.setItem("contraseña", pass.trim());*/
+        location.href="principalmenu.html";
+    }//cierre else
+} //cierre de funcion
