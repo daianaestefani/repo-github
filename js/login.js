2eb0e6973904c540f241b27b9b4aecb2de20ddf3
daianaestefani <69016478+daianaestefani@users.noreply.github.com>
2020-08-23 03:04:50 -0300

    Add files via upload
    REESCRIBIR LOGIN.JS

--- js/login.js
+++ js/login.js
@@ -7,8 +7,17 @@ function loginUser(user, pass){
     if (user.trim()=== "" || pass.trim()=== ""){
         alert("Debe rellenar los datos");
     } else {
-        /*localStorage.setItem("usuario", user.trim());
-        localStorage.setItem("contraseña", pass.trim());*/
+        sessionStorage.setItem("usuario", user.trim());
+        sessionStorage.setItem("contraseña", pass.trim());
         location.href="principalmenu.html";
     }//cierre else
 } //cierre de funcion
+
+//funcion iniciar sesion con GOOGLE
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  
