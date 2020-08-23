c9e12428a83b14e2336eb2e2d4e3265ec22f7f34
daianaestefani <69016478+daianaestefani@users.noreply.github.com>
2020-08-23 04:00:09 -0300

    Add files via upload
    modificando archivo login.js
    para guardar las acciones de los botones iniciar-login-cerrarsesion

--- js/login.js
+++ js/login.js
@@ -20,4 +20,12 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
-  
+
+//funcion cerrar sesion
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+  //<a href="#" onclick="signOut();">Sign out</a>
