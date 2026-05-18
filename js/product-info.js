4537bb0a2dd27917eee1f81f1b0381a1458bf339
daianaestefani <69016478+daianaestefani@users.noreply.github.com>
2026-05-18 19:15:03 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -1,33 +1,46 @@
+// URLs directas de la API de JAP
+const URL_INFO = "https://japceibal.github.io/emercado-api/products/" + localStorage.getItem("prodID") + ".json";
+const URL_COMMENTS = "https://japceibal.github.io/emercado-api/products_comments/" + localStorage.getItem("prodID") + ".json";
+
+// Función para reemplazar getJSONData si falla la carga
+function fetchJSON(url) {
+    return fetch(url).then(response => response.json());
+}
+
 document.addEventListener("DOMContentLoaded", function(e) {
-    // Llamada a la API utilizando la función definida en init.js
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
-        if (resultObj.status === "ok") {
-            let product = resultObj.data;
+    // Intentamos cargar la info del producto
+    fetchJSON(URL_INFO).then(p => {
+        document.getElementById("productName").innerHTML = p.name;
+        document.getElementById("productPrice").innerHTML = p.cost;
+        document.getElementById("productMoneda").innerHTML = p.currency;
+        document.getElementById("proDescription").innerHTML = p.description;
+        document.getElementById("soldCount").innerHTML = p.soldCount;
 
-            // Llenado de información básica
-            document.getElementById("productName").innerHTML = product.name;
-            document.getElementById("productPrice").innerHTML = product.cost;
-            document.getElementById("productMoneda").innerHTML = product.currency;
-            document.getElementById("proDescription").innerHTML = product.description;
-            document.getElementById("soldCount").innerHTML = product.soldCount;
+        // Imágenes
+        let imgHTML = "";
+        p.images.forEach(img => {
+            imgHTML += `<div class="col-3"><img src="${img}" class="img-fluid img-thumbnail"></div>`;
+        });
+        document.getElementById("productImages").innerHTML = imgHTML;
 
-            // Generación de imágenes (asegúrate de que product.images sea un array)
-            let imgHTML = "";
-            product.images.forEach(img => {
-                imgHTML += `<div class="col-3"><img src="${img}" class="img-fluid img-thumbnail"></div>`;
-            });
-            document.getElementById("productImages").innerHTML = imgHTML;
+        // Relacionados
+        let relHTML = "";
+        p.relatedProducts.forEach(rel => {
+            relHTML += `
+            <div class="col-3">
+                <img src="${rel.image}" class="img-fluid">
+                <p>${rel.name}</p>
+            </div>`;
+        });
+        document.getElementById("proRelacionados").innerHTML = relHTML;
+    }).catch(err => console.error("Error cargando el producto:", err));
 
-            // Generación de productos relacionados
-            let relHTML = "";
-            product.relatedProducts.forEach(rel => {
-                relHTML += `
-                <div class="col-3">
-                    <img src="${rel.image}" class="img-fluid">
-                    <p>${rel.name}</p>
-                </div>`;
-            });
-            document.getElementById("proRelacionados").innerHTML = relHTML;
-        }
+    // Intentamos cargar los comentarios
+    fetchJSON(URL_COMMENTS).then(comments => {
+        let commHTML = "";
+        comments.forEach(c => {
+            commHTML += `<li class="list-group-item">${c.user}: ${c.description}</li>`;
+        });
+        document.getElementById("productComments").innerHTML = commHTML;
     });
 });
