c651f77945ffd0cd2c8e2489122231ccb2d75de3
daianaestefani <69016478+daianaestefani@users.noreply.github.com>
2026-05-18 16:14:43 -0300

    Update init.js
    CAMBIO PARA CARGA DE ARCHIVOS JSON

--- js/init.js
+++ js/init.js
@@ -1,13 +1,13 @@
-const PRODUCT_INFO_URL          = "http://localhost:4000/product/5678.json"; /*"https://japdevdep.github.io/ecommerce-api/product/5678.json";*/
-const PUBLISH_PRODUCT_URL       = "http://localhost:4000/product/publish.json";          /*"https://japdevdep.github.io/ecommerce-api/product/publish.json";*/
-const PRODUCT_INFO_COMMENTS_URL = "http://localhost:4000/products/comments";     /*"https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";*/
-const PRODUCTS_URL              = "http://localhost:4000/products";              /*"https://japdevdep.github.io/ecommerce-api/product/all.json";*/
+const PRODUCT_INFO_URL          = "https://japdevdep.github.io/ecommerce-api/product/5678.json"; /*"http://localhost:4000/product/5678.json"; */
+const PUBLISH_PRODUCT_URL       = "https://japdevdep.github.io/ecommerce-api/product/publish.json"; /*"http://localhost:4000/product/publish.json";          */
+const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json"; /*"http://localhost:4000/products/comments";     */
+const PRODUCTS_URL              = "https://japdevdep.github.io/ecommerce-api/product/all.json"; /*"http://localhost:4000/products";              */
 
-const CART_BUY_URL          = "http://localhost:4000/cart/buy.json";                /*"https://japdevdep.github.io/ecommerce-api/cart/buy.json";*/ //cartel, has comprado con exito
-const CART_INFO_URL         = "http://localhost:4000/cart/pino";                 /*"https://japdevdep.github.io/ecommerce-api/cart/987.json";*/ //solo carga elemento pino, no lo uso...
+const CART_BUY_URL          = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";       /*"http://localhost:4000/cart/buy.json";*/                /**/ //cartel, has comprado con exito
+const CART_INFO_URL         = "https://japdevdep.github.io/ecommerce-api/cart/987.json";      /*"http://localhost:4000/cart/pino";       //          */ //solo carga elemento pino, no lo uso...
 
-const CATEGORIES_URL        = "http://localhost:4000/categories";                /*"https://japdevdep.github.io/ecommerce-api/category/all.json";*/
-const CATEGORY_INFO_URL     = "http://localhost:4000/categories/info";           /*"https://japdevdep.github.io/ecommerce-api/category/1234.json";*/
+const CATEGORIES_URL        = "https://japdevdep.github.io/ecommerce-api/category/all.json";  /* "http://localhost:4000/categories";*/
+const CATEGORY_INFO_URL     = "https://japdevdep.github.io/ecommerce-api/category/1234.json";/*"http://localhost:4000/categories/info";           */
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
